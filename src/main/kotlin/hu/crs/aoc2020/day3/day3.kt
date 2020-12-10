0352b0720edef3c607890e262779f3c1c56bb786
Viktor Kovacs <crusader8@gmail.com>
2020-12-10 18:46:10 +0100

    add day 3 solution 2

--- src/main/kotlin/hu/crs/aoc2020/day3/day3.kt
+++ src/main/kotlin/hu/crs/aoc2020/day3/day3.kt
@@ -4,16 +4,24 @@ import java.io.File
 
 fun main() {
     val input = File("src/main/resources/day3.txt").readLines()
-    println(countTrees(input))
+    val right1Down1Trees = countTrees(input, 1, 1)
+    val right3Down1Trees = countTrees(input, 3, 1)
+    val right5Down1Trees = countTrees(input, 5, 1)
+    val right7Down1Trees = countTrees(input, 7, 1)
+    val right1Down2Trees = countTrees(input, 1, 2)
+
+    println(right1Down1Trees)
+    val allTrailsTrees = 1L * right1Down1Trees * right3Down1Trees * right5Down1Trees * right7Down1Trees * right1Down2Trees
+    println(allTrailsTrees)
 }
 
-fun countTrees(map: List<String>): Int {
+fun countTrees(map: List<String>, rightMovement: Int, downMovement: Int): Int {
     var treeCount = 0
 
     var column = 0
 
-    for (row in 1 until map.size) {
-        column += 3
+    for (row in downMovement until map.size step downMovement) {
+        column += rightMovement
 
         if (column >= map[row].length) {
             column -= map[row].length
